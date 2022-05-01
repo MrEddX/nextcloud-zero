b3852b148c145eee91715081eb361c491276936a
MrEddX <66828538+MrEddX@users.noreply.github.com>
2022-05-01 11:20:14 +0300

    Optimize .bash_aliases file creation
    ## Removed
    
    - A **test** checking for existence of `/root/.bash_aliases` file. Either way it will be created, if it does not exist, after the next command is executed.
    
    ## Changes
    
    - Visual: lower to upper case.

--- zero.sh
+++ zero.sh
@@ -185,7 +185,7 @@ RESOLVER=$(cat /etc/resolv.conf | grep "nameserver" | awk '{ print $2 }')
 
 ###########################
 # D: Lokale IP ermitteln  #
-# E: identify local ip    #
+# E: Identify local ip    #
 ###########################
 IPA=$(hostname -I | awk '{print $1}')
 
@@ -1194,7 +1194,6 @@ ${sudo} -u www-data ${touch} /var/log/nextcloud/nextcloud.log
 ###########################
 # occ Aliases (nocc)      #
 ###########################
-if [ ! -f /root/.bash_aliases ]; then ${touch} /root/.bash_aliases; fi
 ${cat} <<EOF >> /root/.bash_aliases
 alias nocc="${sudo} -u www-data /usr/bin/php /var/www/nextcloud/occ"
 EOF
